235ce9514d4975b6ad39d1748e4b1a0eb70febd7
shsha <shsha@DESKTOP-3VO1FS3>
2024-03-19 12:52:30 +0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + "отмечен");
+        }
+        private void checkBox_Uncheked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + "не отмечен");
+        }
+        private void checkBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + "в неопределенном состоянии");
+        }
+    }
+}
